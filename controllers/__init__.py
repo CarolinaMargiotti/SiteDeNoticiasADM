26a3960951106616c23ef33587d04353bca16e7b
Carolina M <carolinamargiott2i@gmail.com>
2022-03-13 16:56:41 -0300

    terminado basico da tela de home, criar e editar post

--- controllers/__init__.py
+++ controllers/__init__.py
@@ -2,6 +2,8 @@ import os
 from flask import Blueprint, current_app
 
 from controllers.news_controller import home as news_home
+from controllers.news_controller import createNews as news_create
+from controllers.news_controller import editNews as news_edit
 
 template_dir = os.path.abspath('sitedenoticiasadm/templates/')
 
@@ -10,3 +12,11 @@ news_bp = Blueprint("news",__name__,template_folder=template_dir)
 @news_bp.route("/")
 def home():
     return news_home()
+
+@news_bp.route("/create")
+def createNews():
+    return news_create()
+
+@news_bp.route("/edit/<int:postId>")
+def editNews(postId):
+    return news_edit(postId)
--- controllers/news_controller.py
+++ controllers/news_controller.py
@@ -1,5 +1,35 @@
 from flask import request, jsonify, render_template
 
+
+noticias = [
+{'id':0,'titulo':"porquinhos da india",'assunto':"pets",'conteudo':"cuicuicuicuicui"},
+{'id':1,'titulo':"cobrinha",'assunto':"pets",'conteudo':"ssssssssssssss"},
+{'id':2,'titulo':"macaco",'assunto':"pets",'conteudo':"uh uh uh uh"},
+{'id':3,'titulo':'art nouvelle','assunto':'arte','conteudo':'arte dos elfos'},
+{'id':4,'titulo':'arte barroca','assunto':'arte','conteudo':'materia da escola'},
+{'id':5,'titulo':'lançamento elden ring','assunto':'geek','conteudo':'é um jogo que lançou'},
+{'id':6,'titulo':'sonic é bom','assunto':'geek','conteudo':'gotta go fast'},
+]
+
+assuntos = ['pets','arte','geek']
+
+def getNews():
+    return noticias
+
+def getSubjects():
+    return assuntos
+
 #retorno dos templates
+
 def home():
-    return render_template("home.html")
+    noticias = getNews()
+    return render_template("home.html",newsList=noticias)
+
+def createNews():
+    assuntos = getSubjects()
+    return render_template("createNewsPost.html",subjects=assuntos)
+
+def editNews(postId):
+    noticia = getNews()[postId]
+    assuntos = getSubjects()
+    return render_template("editNewsPost.html",news=noticia,subjects=assuntos)
