773059a98b1505d1d4a1681be4d31ffad7397943
Carolina M <carolinamargiott2i@gmail.com>
2022-03-21 20:02:32 -0300

    trocada as noticias padrão de visualização, colocado para display resumo inves de conteudo na tabela e criado um news model

--- controllers/news_controller.py
+++ controllers/news_controller.py
@@ -1,15 +1,5 @@
 from flask import request, jsonify, render_template
-
-
-noticias = [
-{'id':0,'titulo':"porquinhos da india",'assunto':"pets",'conteudo':"cuicuicuicuicui"},
-{'id':1,'titulo':"cobrinha",'assunto':"pets",'conteudo':"ssssssssssssss"},
-{'id':2,'titulo':"macaco",'assunto':"pets",'conteudo':"uh uh uh uh"},
-{'id':3,'titulo':'art nouvelle','assunto':'arte','conteudo':'arte dos elfos'},
-{'id':4,'titulo':'arte barroca','assunto':'arte','conteudo':'materia da escola'},
-{'id':5,'titulo':'lançamento elden ring','assunto':'geek','conteudo':'é um jogo que lançou'},
-{'id':6,'titulo':'sonic é bom','assunto':'geek','conteudo':'gotta go fast'},
-]
+from models.news import news as noticias
 
 assuntos = ['pets','arte','geek']
 
